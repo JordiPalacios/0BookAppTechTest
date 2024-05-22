bfa7801e41352d423c3e95ee6ea5c66e1887d5df
JordiPalacios <jordipalacioseso@gmail.com>
2024-05-22 07:51:52 +0200

    componente filtrado y seleccion por genero empezado

--- src/BookApp.jsx
+++ src/BookApp.jsx
@@ -1,14 +1,53 @@
+import { useState } from "react";
 import "./App.css";
+import BooksFilter from "./components/BooksFilter";
 import ItemContainer from "./components/ItemContainer";
 import SectionContainer from "./components/SectionContainer";
 import books from "./mock/books.json";
 
 export default function BookApp() {
+    const [gender, setGender] = useState("Todos")
+
+    const GENDER = [
+        {
+            name: "Todos",
+        },
+        {
+            name: "Hombre",
+        },
+        {
+            name: "Mujer",
+        },
+        {
+            name: "Otros",
+        },
+    ];
+  
+    const handleClick = (genderValue) => {
+      let gender = genderValue.target.value
+
+      if (gender === "Hombre") {
+        setGender("Hombre");
+    } else if (gender === "Mujer") {
+        setGender("Mujer");
+    } else if (gender ==="Otros") {
+        setGender("Otros");
+    } else {
+        setGender("Todos");
+    }
+  }
+
     return (
         <>
             <h1 className="text-5xl font-semibold underline mb-6 text-emerald-500">
                 BooksApp
             </h1>
+              {GENDER.map((entry, index) => (
+                <button key={index} onClick={handleClick} value={entry.name}>
+                  {entry.name}
+                </button>
+              ))}
+            <BooksFilter genderValue={gender} />
             <SectionContainer sectionClassName="text-white/90">
                 {books.library.map((entry, index) => (
                     <ItemContainer key={index}>
