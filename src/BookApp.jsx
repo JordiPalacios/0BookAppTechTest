229ad6bf10a88adbeeec556259d86a01d7db1d5a
JordiPalacios <jordipalacioseso@gmail.com>
2024-05-22 07:19:34 +0200

    prettier executed

--- src/BookApp.jsx
+++ src/BookApp.jsx
@@ -1,21 +1,29 @@
-import './App.css'
-import ItemContainer from './components/ItemContainer'
-import SectionContainer from './components/SectionContainer'
-import books from './mock/books.json'
+import "./App.css";
+import ItemContainer from "./components/ItemContainer";
+import SectionContainer from "./components/SectionContainer";
+import books from "./mock/books.json";
 
 export default function BookApp() {
-  return (
-    <>
-      <h1 className='text-5xl font-semibold underline mb-6 text-emerald-500'>BooksApp</h1>
-      <SectionContainer sectionClassName="text-white/90">
-          {books.library.map((entry, index) => (
-              <ItemContainer key={index}>
-                <h3 className=' text-2xl font-semibold text-white/80'>{entry.book.title}</h3>
-                <img className='max-w-xs h-auto p-4' src={entry.book.cover} alt={`img${entry.book.title}`} />
-                <article>{entry.book.synopsis}</article>
-              </ItemContainer>
-            ))}
-      </SectionContainer>
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-5xl font-semibold underline mb-6 text-emerald-500">
+                BooksApp
+            </h1>
+            <SectionContainer sectionClassName="text-white/90">
+                {books.library.map((entry, index) => (
+                    <ItemContainer key={index}>
+                        <h3 className=" text-2xl font-semibold text-white/80">
+                            {entry.book.title}
+                        </h3>
+                        <img
+                            className="max-w-xs h-auto p-4"
+                            src={entry.book.cover}
+                            alt={`img${entry.book.title}`}
+                        />
+                        <article>{entry.book.synopsis}</article>
+                    </ItemContainer>
+                ))}
+            </SectionContainer>
+        </>
+    );
 }
--- src/components/ItemContainer.jsx
+++ src/components/ItemContainer.jsx
@@ -1,7 +1,10 @@
-export default function ItemContainer ({ index, children }) {
+export default function ItemContainer({ index, children }) {
     return (
-        <div key={index} className="flex flex-col items-center justify-between m-6 overflow-hidden">
+        <div
+            key={index}
+            className="flex flex-col items-center justify-between m-6 overflow-hidden"
+        >
             {children}
         </div>
-    )
+    );
 }
