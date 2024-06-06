dbcbeffaad0bf72cf097805c85b33819cf2a01bd
JordiPalacios <jordipalacioseso@gmail.com>
2024-06-06 07:54:25 +0200

    content prettier

--- src/components/SectionContainer.jsx
+++ src/components/SectionContainer.jsx
@@ -1,7 +1,7 @@
 export default function SectionContainer({ sectionClassName, children }) {
     return (
         <div
-            className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 auto-cols-fr autoco  text-pretty gap-4 my-6 mx-6 ${sectionClassName}`}
+            className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 auto-cols-fr  text-pretty gap-4 my-6 mx-6 ${sectionClassName}`}
         >
             {children}
         </div>
